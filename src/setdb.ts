ac87fe9a5d7f8d04e800a207656a8389aac7e588
nick11967 <nick11967@snu.ac.kr>
2024-03-17 14:09:52 +0900

    Move Schedule data to env

---
+++ src/setdb.ts
@@ -0,0 +1,32 @@
+import "discord.js";
+import { env } from "process";
+import ScheModel from "./schemas/schedule";
+import ClearCollections from "./db";
+import { i2nModel } from "./schemas/id2nick";
+
+if (!env.SEASON3_SCHEDULES) {
+  throw new Error("SEASON3_SCHEDULES is not defined");
+}
+const schedules = env.SEASON3_SCHEDULES.split(",").map((s) => {
+  const [id, offday] = s.split(":");
+  return { id, offday };
+});
+
+const setupdb = async (guild: any) => {
+  await ClearCollections();
+  ScheModel.insertMany(schedules);
+  console.log("schedules inserted");
+  guild.members.fetch().then((members: any) => {
+    members.forEach((member: any) => {
+      let { nickname } = member;
+      if (!nickname) {
+        nickname = member.user.displayName;
+      }
+      const mem = { id: member.user.username, nick: nickname };
+      i2nModel.create(mem);
+    });
+  });
+  console.log("id2nicks inserted");
+};
+
+export default setupdb;
